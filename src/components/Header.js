2b62234dbaa7ee0289ed6a49dc34f435852dad57
kasimurkmez <114136293+Kasimurkmez@users.noreply.github.com>
2023-02-24 03:01:40 +0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <div className="container-fluid bg-light d-flex justify-content-center align-items-center">
-      <h2 className="m-0 text-primary text-center mt-4">Çalışanlarımız</h2>
+      <h2 className="m-0 text-primary text-center mt-4">Our employees</h2>
     </div>
   );
 }
